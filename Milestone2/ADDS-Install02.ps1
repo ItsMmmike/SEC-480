2516e6360ecf997a1d30ceec9ff383038c3ddd77
ItsMmmike <113321364+ItsMmmike@users.noreply.github.com>
2026-02-10 15:51:58 -0500

    Create ADDS-Install02.ps1

---
+++ Milestone2/ADDS-Install02.ps1
@@ -0,0 +1,15 @@
+# Pt.2 Script used to setup and deploy DNS and DHCP on Windows Server Host (Post AD Install)
+
+## Start Post-Install Setup for DNS
+Add-DnsServerPrimaryZone -Name "$domain" -ReplicationScope "Forest" -PassThru
+Add-DnsServerPrimaryZone -NetworkID "10.0.17.0/24" -ReplicationScope "Forest"
+
+# Configure additional DNS Entries for Server
+Add-DnsServerResourceRecordA -Name "fw01-480" -ZoneName "$domain" -IPv4Address "10.0.17.2" -CreatePtr # FW
+Add-DnsServerResourceRecordA -Name "vcenter" -ZoneName "$domain" -IPv4Address "10.0.17.3" -CreatePtr # vCenter
+Add-DnsServerResourceRecordA -Name "mgmt01-copper" -ZoneName "$domain" -IPv4Address "10.0.17.100" -CreatePtr # Mgmt
+Add-DnsServerResourceRecordPtr -Name "4" -ZoneName "17.0.10.in-addr.arpa" -PtrDomainName "ad01-steel.$domain" # AD
+
+## Start Post-Install Setup for DHCP
+Add-DhcpServerv4Scope -Name "Infra DHCP" -StartRange 10.0.17.101 -EndRange 10.0.17.150 -SubnetMask 255.255.255.0 -State Active
+Set-DhcpServerv4OptionValue -ScopeId 10.10.17.0 -Router 10.0.17.2 -DnsServer 10.0.17.4
