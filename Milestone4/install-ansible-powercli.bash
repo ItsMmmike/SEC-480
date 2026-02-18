41ea636f0b03f98535c31e6765d64200d06a2cdf
ItsMmmike <113321364+ItsMmmike@users.noreply.github.com>
2026-02-18 10:25:07 -0500

    Update and rename install-ansible.bash to install-ansible-powercli.bash

--- Milestone4/install-ansible-powercli.bash
+++ Milestone4/install-ansible-powercli.bash
@@ -1,5 +1,5 @@
-# Script used to install ansible onto a ubuntu based system
-## To Use: Download Script from this repo, then run with "sudo bash ./install.ansible.bash"
+# Script used to install ansible and PowerCLI tools onto a ubuntu based system
+## To Use: Download Script from this repo, then run with "sudo bash ./install-ansible-powercli.bash"
 
 # Install Ansible Dependencies
 sudo apt install sshpass python3-paramiko git
@@ -7,6 +7,11 @@ sudo apt-add-repository ppa:ansible/ansible
 sudo apt update
 sudo apt install ansible
 ansible --version
+
+# Install PowerShell, VSCode, remmina, and onboard
+sudo snap install powershell --classic
+sudo snap install code --classic
+sudo apt install remmina onboard
 
 # Set Ansible to ignore checking host keys before connecting to target systems
 cat >> ~/.ansible.cfg << EOF                                                               
