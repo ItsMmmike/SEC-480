7a0ef6f69d726fb78224e96e870a5b2838f26932
ItsMmmike <113321364+ItsMmmike@users.noreply.github.com>
2026-05-01 23:11:32 -0400

    Update deploy_ou.ps1

--- Milestone9/deploy_ou.ps1
+++ Milestone9/deploy_ou.ps1
@@ -4,8 +4,8 @@ Import-Module ActiveDirectory
 New-ADOrganizationalUnit -Name "Blue21" -Path "DC=blue21,DC=local"
 
 New-ADOrganizationalUnit -Name "Accounts" -Path "DC=blue21,DC=local"
-New-ADOrganizationalUnit -Name "Groups" -Path "OU=Accounts,DC=blue21,DC=local"
+New-ADOrganizationalUnit -Name "Groups" -Path "OU=Accounts,OU=Blue21,DC=blue21,DC=local"
 New-ADOrganizationalUnit -Name "Computers" -Path "DC=blue21,DC=local"
-New-ADOrganizationalUnit -Name "Servers" -Path "OU=Computers,DC=blue21,DC=local"
-New-ADOrganizationalUnit -Name "Workstations" -Path "OU=Computers,DC=blue21,DC=local"
+New-ADOrganizationalUnit -Name "Servers" -Path "OU=Computers,OU=Blue21,DC=blue21,DC=local"
+New-ADOrganizationalUnit -Name "Workstations" -Path "OU=Computers,OU=Blue21,DC=blue21,DC=local"
 
